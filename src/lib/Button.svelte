9e3ecf34be92fdd37d8091ba3677c708e90e10de
moon in learn <mooninlearn@gmail.com>
2023-06-09 17:38:52 +0900

    14. Adding Classes Dynamically Using The Class Directive

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -6,6 +6,7 @@
 <button 
   class:size-lg={size === 'large'}
   class:size-sm={size === 'small'}
+  class:shadow={shadow}
 ><slot>Default Text</slot></button>
 
 <style lang="scss">
@@ -16,12 +17,21 @@
     font-weight: bold;
     border-radius: 5px;
     cursor: pointer;
+    &:hover {
+      background-image: linear-gradient(rgba(0,0,0,0.4) 0 0);
+    }
+    &:active {
+      background-image: linear-gradient(rgba(255, 255, 255, 0.2) 0 0);
+    }
     &.size-sm {
       padding: 15px 20px;
     }
     &.size-lg {
       padding: 20px 30px;
       font-size: 20px;
+    }
+    &.shadow {
+      box-shadow: 0 0 10px rgba(1,1,1,0.6);
     }
   }
 </style>
