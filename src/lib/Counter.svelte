1dcd8355fbe1903819b3f836d7e6acf5ad881e0b
moon in learn <mooninlearn@gmail.com>
2023-06-09 14:50:24 +0900

    10. Reactive Statements Gotchas > getTotal()

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,17 +1,23 @@
 <script>
-    let count = 0;
+    let count1 = 0;
+    let count2 = 0;
     let string;
-    $: string = `You have clicked ${count} times.`;
 
-    $: if (count > 5) {
-        console.log(`The count is ${count}`)
+    function getTotal(x, y) {
+        return x + y
     }
-    const increment = () => {
-        count += 1;
+    $: string = `The count total is ${getTotal(count1, count2)}.`;
+
+    const increment1 = () => {
+        count1 += 1;
+    }
+    const increment2 = () => {
+        count2 += 1;
     }
 </script>
 
-<button on:click={increment}>Clicks {count}</button>
+<button on:click={increment1}>Clicks {count1}</button>
+<button on:click={increment2}>Clicks {count2}</button>
 <h3>{string}</h3>
 
 <style>
