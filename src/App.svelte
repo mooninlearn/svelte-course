36aa6bf21c34a49b9b28eec7169b1147a4e5f934
moon in learn <mooninlearn@gmail.com>
2023-06-09 16:12:51 +0900

    14. Adding Classes Dynamically Using The Class Directive: 1. 삼항연산자 이용

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,7 @@
     import Button from './lib/Button.svelte';
 </script>
 
-<Button>
-    Button Text
-</Button>
+<Button size="large" shadow>Button Text</Button>
 
 <style>
 </style>
--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,19 +1,24 @@
 <script>
+  export let size = 'small';
+  export let shadow = false;
 </script>
 
-<button><slot>Default Text</slot></button>
+<button class={size === 'large' ? "size-lg" : "size-sm"}><slot>Default Text</slot></button>
 
 <style lang="scss">
   button {
     border: none;
     background-color: #ff3e00;
     color: #ffffff;
-    padding: 15px 20px;
     font-weight: bold;
     border-radius: 5px;
     cursor: pointer;
-    &:hover {
-      background-color: variables.$color;
+    &.size-sm {
+      padding: 15px 20px;
+    }
+    &.size-lg {
+      padding: 20px 30px;
+      font-size: 20px;
     }
   }
 </style>
