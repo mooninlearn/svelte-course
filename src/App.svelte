508f0666b7f227fc72686242cfb48b8f093f1342
moon in learn <mooninlearn@gmail.com>
2023-06-09 15:19:17 +0900

    11. Component Props

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
 <script>
     import Counter from './lib/Counter.svelte';
+
+    // const maxCount = 6;
+    const props = {
+        maxCount: 10,
+        initialCount: 5
+    }
 </script>
 
-<Counter />
+<!-- <Counter initialCount={3} {maxCount}/>
+ -->
+<Counter {...props} x="y" />
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,24 +1,20 @@
 <script>
-    let count1 = 0;
-    let count2 = 0;
-    let string;
+    export let initialCount;
+    export let maxCount = undefined;
 
-    function getTotal(x, y) {
-        return x + y
-    }
-    $: string = `The count total is ${getTotal(count1, count2)}.`;
+    console.log($$props, $$restProps);
 
-    const increment1 = () => {
-        count1 += 1;
+    let count = initialCount;
+
+    const increment = () => {
+        if (count === maxCount) return;
+        count += 1;
     }
-    const increment2 = () => {
-        count2 += 1;
-    }
+
 </script>
 
-<button on:click={increment1}>Clicks {count1}</button>
-<button on:click={increment2}>Clicks {count2}</button>
-<h3>{string}</h3>
+<button on:click={increment}>Clicks {count}</button>
+
 
 <style>
     button {
